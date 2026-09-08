49be840c50f1562128c55574e29c7846b88bb152
小小梦工场 <1113638674@qq.com>
2026-09-08 23:37:25 +0800

    chore: 本地联调地址 192.168.31.138 → 192.168.31.139（本机局域网 IP 变更）

--- miniprogram/config/index.js
+++ miniprogram/config/index.js
@@ -21,7 +21,7 @@ module.exports = {
     const FORCE_ONLINE = false;
     const isOnline = FORCE_ONLINE || envVersion === 'release' || envVersion === 'trial';
     // 路径前缀 /sport-track 与同域名其他服务区分（nginx 按前缀转发）
-    return isOnline ? 'https://api.historybook.cn/sport-track' : 'http://192.168.31.138:3004/sport-track';
+    return isOnline ? 'https://api.historybook.cn/sport-track' : 'http://192.168.31.139:3004/sport-track';
   })(),
 
   /**
